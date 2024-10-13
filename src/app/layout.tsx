56886fbae9069a28a10a691db25ea14ed6f9f829
Marcelllombm <melombm@hotmail.com>
2024-10-13 07:59:23 -0300

    feat: NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { NavBar } from "@/components";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <p>Root layout</p>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex gap-2`} >
+        <NavBar/>
         {children}
       </body>
     </html>
